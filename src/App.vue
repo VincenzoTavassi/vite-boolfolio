29ce6307800a89e1a45ddde9c63839e63c82addf
VincenzoTavassi <info@egadivacanze.it>
2023-04-21 19:31:26 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppNavbar from "./components/AppNavbar.vue";
+import ProjectList from "./components/ProjectList.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: { AppNavbar, ProjectList },
+};
+</script>
+
+<template>
+  <header>
+    <AppNavbar></AppNavbar>
+  </header>
+  <main class="container">
+    <h1>{{ title }}</h1>
+    <ProjectList />
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
