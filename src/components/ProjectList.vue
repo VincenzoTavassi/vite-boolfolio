44e0c020a106af98ef6b95f16f01935d8bbffe10
VincenzoTavassi <info@egadivacanze.it>
2023-04-21 19:31:44 +0200

    add project list

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,34 @@
+<script>
+import axios, { Axios } from "axios";
+import ProjectCard from "./ProjectCard.vue";
+
+export default {
+  data() {
+    return {
+      projects: [],
+      pages: [],
+    };
+  },
+
+  methods: {
+    fetchProjects() {
+      axios.get("http://localhost:8000/api/projects").then((response) => {
+        this.projects = response.data.projects.data;
+        this.pages = response.data.projects.links;
+      });
+    },
+  },
+  components: { ProjectCard },
+  created() {
+    this.fetchProjects();
+  },
+};
+</script>
+
+<template>
+  <div class="row row-cols-3">
+    <ProjectCard v-for="project in projects" :project="project"></ProjectCard>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
