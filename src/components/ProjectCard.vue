cec67ea931c0bdb3cd11eed8466a4f03d0ff021a
VincenzoTavassi <info@egadivacanze.it>
2023-04-21 19:31:53 +0200

    add project card with badges

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,47 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+  props: { project: Object },
+
+  created() {
+    console.log(this.project);
+  },
+};
+</script>
+
+<template>
+  <div class="col my-2">
+    <div class="card h-100">
+      <img
+        :src="project.image ? project.image : project.link"
+        class="card-img-top"
+        :alt="project.title"
+      />
+      <div class="card-body d-flex flex-column align-items-center">
+        <h5 class="card-title">{{ project.title }}</h5>
+        <p>
+          <strong>Tipo: </strong>
+          <span v-html="project.type.badge"></span>
+        </p>
+
+        <p class="card-text">{{ project.description }}</p>
+        <p>
+          <strong>Tecnologie: </strong>
+          <span
+            v-for="technology in project.technologies"
+            v-html="technology.badge"
+          ></span>
+        </p>
+        <!-- <a href="{{route('guest.show', $project)}}" class="btn btn-dark mt-auto">Dettagli</a> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
