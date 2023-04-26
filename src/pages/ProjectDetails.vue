521a5d19495c1fd348468f36f0d43bc27d12d39e
VincenzoTavassi <info@egadivacanze.it>
2023-04-26 18:43:46 +0200

    project details

---
+++ src/pages/ProjectDetails.vue
@@ -0,0 +1,61 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      project: "",
+    };
+  },
+  methods: {
+    fetchProject() {
+      axios
+        .get(`http://localhost:8000/api/projects/${this.$route.params.id}`)
+        .then((response) => {
+          console.log(response.data);
+          this.project = response.data;
+        });
+    },
+  },
+  created() {
+    this.fetchProject();
+  },
+};
+</script>
+
+<template>
+  <div class="col my-2">
+    <div class="card">
+      <div class="d-flex card-body">
+        <div class="d-flex flex-column justify-content-center">
+          <h5 class="card-title">{{ project.title }}</h5>
+          <p>
+            <strong>Tipo: </strong>
+            <span v-html="project.type.badge"></span>
+          </p>
+
+          <p class="card-text">{{ project.description }}</p>
+          <p>
+            <strong>Tecnologie: </strong>
+            <span
+              v-for="technology in project.technologies"
+              v-html="technology.badge + ' '"
+            ></span>
+          </p>
+        </div>
+        <img
+          :src="project.image ? project.image : project.link"
+          class="w-50 me-3"
+          :alt="project.title"
+        />
+      </div>
+    </div>
+  </div>
+  <div class="d-flex justify-content-center">
+    <RouterLink class="btn btn-dark btn-sm" :to="{ name: 'home' }"
+      >Torna alla Home</RouterLink
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
